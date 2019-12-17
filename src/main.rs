6990ed86801692aaa29390285add8d06fbe7ce82
Tiberio Ferreira <tiberiusferreira@gmail.com>
2019-12-17 08:07:45 -0300

    Can train on samples

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
-mod preprocessing;
+use std::fs::File;
+
+use imageproc::drawing::Blend;
 use rand::Rng;
 use tch::nn::{Module, ModuleT, OptimizerConfig};
 use tch::rust_image::resize_img;
-use tch::vision::image;
-use tch::vision::imagenet::save_image;
-use tch::{kind, nn, no_grad, vision, Device, Kind, Reduction, Tensor};
+use tch::{kind, nn, no_grad, vision, Device, IndexOp, Kind, Reduction, Tensor};
+
+use crate::preprocessing::{
+    draw_bb_to_img, from_tensor, objects_mask_tensor_from_target_tensor, to_tensor
+};
+use std::process::exit;
+use crate::preprocessing::structs::CleanedImgLabels;
+use tch::vision::image::resize;
+
+mod preprocessing;
 
 const NB_CLASSES: i64 = 1;
 
@@ -15,20 +24,35 @@ fn net(vs: &nn::Path) -> impl ModuleT {
     };
     nn::seq_t()
         .add(nn::conv2d(vs, 3, 16, 3, conv_cfg))
+        .add(nn::batch_norm2d(vs, 16, Default::default()))
         .add_fn(|x| x.leaky_relu()) // change to slope 0.1
         .add_fn(|x| x.max_pool2d_default(2))
         .add(nn::conv2d(vs, 16, 32, 3, conv_cfg))
+        .add(nn::batch_norm2d(vs, 32, Default::default()))
         .add_fn(|x| x.leaky_relu()) // change to slope 0.1
         .add_fn(|x| x.max_pool2d_default(2))
         .add(nn::conv2d(vs, 32, 64, 3, conv_cfg))
+        .add(nn::batch_norm2d(vs, 64, Default::default()))
         .add_fn(|x| x.leaky_relu()) // change to slope 0.1
         .add_fn(|x| x.max_pool2d_default(2))
+        .add(nn::conv2d(vs, 64, 128, 3, conv_cfg))
+        .add(nn::batch_norm2d(vs, 128, Default::default()))
+        .add_fn(|x| x.leaky_relu()) // change to slope 0.1
+        .add_fn(|x| x.max_pool2d_default(2))
+        .add(nn::conv2d(vs, 128, 256, 3, conv_cfg))
+        .add(nn::batch_norm2d(vs, 256, Default::default()))
+        .add_fn(|x| x.leaky_relu()) // change to slope 0.1
+        .add_fn(|x| x.max_pool2d_default(2))
+        .add(nn::conv2d(vs, 256, 512, 3, conv_cfg))
+        .add(nn::batch_norm2d(vs, 512, Default::default()))
+        .add_fn(|x| x.leaky_relu()) // change to slope 0.1
+        //        .add_fn(|x| x.max_pool2d(&[2, 2], &[1, 1], &[0, 0], &[1, 1], false))
         // Output is (x, y, w, h, object_prob, class1_prob, class2_prob, ...)
-        .add(nn::conv2d(vs, 64, NB_CLASSES + 5, 1, Default::default()))
+        .add(nn::conv2d(vs, 512, NB_CLASSES + 5, 1, Default::default()))
         .add_fn(|x| x.shallow_clone()) // Linear activation
         // normalize output of probabilities
         .add_fn(|x| {
-            let (batch, features, _, _) = x.size4().unwrap();
+            let (_batch, features, _, _) = x.size4().unwrap();
             let nb_classes = features - 5;
 
             // Center x and y
@@ -43,34 +67,79 @@ fn net(vs: &nn::Path) -> impl ModuleT {
 }
 
 fn main() -> failure::Fallible<()> {
+
+
+
     let mut store = nn::VarStore::new(Device::Cpu);
     let net = net(&store.root());
-    let mut opt = nn::Adam::default().build(&store, 1e-3)?;
+    let mut opt = nn::Adam::default().build(&store, 5. * 1e-3)?;
 
-    let original_image = image::load("test.jpg")?;
+    let label_file = File::open("processed_imgs/labels.json").unwrap();
+    let labels: Vec<CleanedImgLabels> = serde_json::from_reader(label_file).unwrap();
+    store.load("variables.ot").unwrap();
+    for i in 0..1 {
+        for label in &labels {
+            let original_image = tch::vision::image::load(format!("processed_imgs/{}", label.img_filename))?;
 
-    let resized_img = image::resize(&original_image, 416, 416).unwrap();
-    let img_as_batch = resized_img.unsqueeze(0).to_kind(tch::Kind::Float) / 255.;
+            let img_as_batch = original_image.unsqueeze(0).to_kind(tch::Kind::Float) / 255.;
 
-    println!("Input: {:?}", img_as_batch.size());
-    let img_as_batch = img_as_batch.set_requires_grad(true);
+//            println!("Input: {:?}", img_as_batch.size());
+            let img_as_batch = img_as_batch.set_requires_grad(true);
 
-    let desired = Tensor::zeros(&[1, 6, 52, 52], (Kind::Float, Device::Cpu));
+            let desired = to_tensor(&label.bboxes, 13, 416, (70, 70))
+                .tensor
+                .unsqueeze(0);
 
-    for i in 0..5 {
-        let output = net.forward_t(&img_as_batch, true);
-        let loss = output.mse_loss(&desired, Reduction::Sum);
-        opt.backward_step(&loss);
-        //        println!("Output {:?}", output.size());
-        println!("Loss {:?}", loss);
+            for i in 0..2 {
+                let output = net.forward_t(&img_as_batch, true);
+                let target_object_prob = desired.narrow(1, 4, 1);
+                let output_object_prob = output.narrow(1, 4, 1);
+                let objectness_loss = output_object_prob.binary_cross_entropy::<Tensor>(
+                    &target_object_prob,
+                    None,
+                    Reduction::Sum,
+                );
+
+                let grid_points_with_obj =
+                    objects_mask_tensor_from_target_tensor(desired.squeeze().into(), 13, 416, (70, 70));
+                let mut others_loss: Tensor = Tensor::from(0.);
+                for point in grid_points_with_obj {
+                    let target_others = desired
+                        .narrow(1, 0, 4)
+                        .narrow(2, point.x, 1)
+                        .narrow(3, point.y, 1);
+                    let output_others = output
+                        .narrow(1, 0, 4)
+                        .narrow(2, point.x, 1)
+                        .narrow(3, point.y, 1);
+
+                    others_loss += output_others.mse_loss(&target_others, Reduction::Sum);
+                }
+                let loss = objectness_loss + others_loss;
+                opt.backward_step(&loss);
+                println!("Loss {:?}", loss);
+            }
+        }
     }
+
+    store.save("variables.ot").unwrap();
+
+    let original_image = tch::vision::image::load("new_samples/image0005_resized.jpg")?;
+
+    let img_as_batch = original_image.unsqueeze(0).to_kind(tch::Kind::Float) / 255.;
     let output = net.forward_t(&img_as_batch, true);
-    output
-        .narrow(1, 0, 6)
-        .narrow(2, 10, 1)
-        .narrow(3, 10, 1)
-        .print();
-    println!("Desired {:?}", desired.size());
+    let out = output.squeeze();
+    let bb = from_tensor(out.into(), 13, 416, (70, 70));
+//        let bb = from_tensor(desired.squeeze().into(), 13, 416, (70, 70));
+
+    let img = image::open("new_samples/image0005_resized.jpg").unwrap();
+    let mut blend = Blend(img.to_rgba());
+    for single_bb in &bb {
+        draw_bb_to_img(&mut blend, single_bb);
+    }
+    blend.0.save("maybe_worked5.jpg").unwrap();
+    println!("{:?}", bb);
+    //    println!("Desired {:?}", desired.size());
 
     Ok(())
 }
--- src/preprocessing.rs
+++ src/preprocessing.rs
@@ -1,27 +1,22 @@
-mod structs;
+pub mod structs;
 
 use image::RgbaImage;
 use imageproc::drawing::Blend;
 use serde::{Deserialize, Serialize};
 use std::fs::File;
 use structs::*;
-use tch::{Device, IndexOp, Kind, Tensor};
+use tch::{Device, IndexOp, Kind, R3TensorGeneric, R4TensorGeneric, Tensor};
 
-const INPUT_FILE: &str = "imgs/export.json";
+const INPUT_FILE: &str = "samples/export.json";
 //const OUTPUT_FILE: &str = "imgs/cleaned.json";
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct CleanedImgLabels {
-    pub img_filename: String,
-    pub bboxes: Vec<Bbox>,
-}
 
 pub fn to_tensor(
-    bbs: Vec<RawBbox>,
+    bbs: &Vec<Bbox>,
     grid_size: u32,
     original_img_size: u32,
     anchors: (u32, u32),
-) -> Tensor {
+) -> R3TensorGeneric {
     let (anchor_width, anchor_height) = anchors;
     let tensor = tch::Tensor::zeros(
         &[6, grid_size as i64, grid_size as i64],
@@ -37,27 +32,29 @@ pub fn to_tensor(
         let width = (bb.width as f32 / anchor_width as f32).ln();
         let height = (bb.height as f32 / anchor_height as f32).ln();
         // find out in which grid (x and y) the object center is
-        let bb_left_center_percentage = (bb.left as f32 / original_img_size as f32); // something like 0.3
-        let grids_as_perc = (1. / grid_size as f32);
-        let grids_cells_to_left = (bb_left_center_percentage / grids_as_perc).floor() as u32;
+        let bb_left_center_percentage = (bb.left + (bb.width/2) as i32) as f32 / original_img_size as f32; // something like 0.3
+        let grid_cell_as_perc = 1. / grid_size as f32;
+        let grids_cells_to_left = (bb_left_center_percentage / grid_cell_as_perc).floor() as u32;
 
-        let bb_top_center_percentage = (bb.top as f32 / original_img_size as f32); // something like 0.3
-        let grids_cells_to_top = (bb_top_center_percentage / grids_as_perc).floor() as u32;
+        let bb_top_center_percentage = (bb.top + (bb.height/2) as i32) as f32 / original_img_size as f32; // something like 0.3
+        let grids_cells_to_top = (bb_top_center_percentage / grid_cell_as_perc).floor() as u32;
 
         tensor
             .i(0 as i64)
             .i(grids_cells_to_left as i64)
             .i(grids_cells_to_top as i64)
             .copy_(&Tensor::from(
-                (bb.left as u32 - grids_cells_to_left * grid_cell_size_pixels + bb.width / 2)
-                    as f32,
+                ((bb.left + bb.width as i32/2) as u32 - grids_cells_to_left * grid_cell_size_pixels)
+                    as f32
+                    / grid_cell_size_pixels as f32,
             ));
         tensor
             .i(1 as i64)
             .i(grids_cells_to_left as i64)
             .i(grids_cells_to_top as i64)
             .copy_(&Tensor::from(
-                (bb.top as u32 - grids_cells_to_top * grid_cell_size_pixels + bb.height / 2) as f32,
+                ((bb.top + bb.height as i32/2) as u32 - grids_cells_to_top * grid_cell_size_pixels) as f32
+                    / grid_cell_size_pixels as f32,
             ));
         tensor
             .i(2 as i64)
@@ -81,10 +78,56 @@ pub fn to_tensor(
             .copy_(&Tensor::from(1.));
     }
 
-    tensor
+    tensor.into()
 }
 
-pub fn from_tensor(tensor: Tensor, grid_size: u32, original_img_size: u32, anchors: (u32, u32)) -> Vec<Bbox>{
+pub struct GridPointsWithObjects {
+    pub x: i64,
+    pub y: i64,
+    pub prob: f64,
+}
+pub fn objects_mask_tensor_from_target_tensor(
+    tensor: R3TensorGeneric,
+    grid_size: u32,
+    original_img_size: u32,
+    anchors: (u32, u32),
+) -> Vec<GridPointsWithObjects> {
+    let tensor = tensor.tensor;
+    let grid_size = grid_size as i64;
+    let (anchor_width, anchor_height) = anchors;
+    let grid_cell_size_pixels: i64 = (original_img_size / grid_size as u32) as i64;
+    // Tensor should be Rank 3: [6, grid_size, grid_size]
+    //                     0        1       2      3          4            5
+    // Tensor layout is CenterX, CenterY, Width, Height, object_prob, class1_prob
+    // select points where object_prob > 0.5
+
+    let mut grid_points_with_objects: Vec<GridPointsWithObjects> = vec![];
+    let objectness_tensor = tensor.i(4 as i64);
+    for x in 0i64..grid_size {
+        for y in 0i64..grid_size {
+            let objectness = objectness_tensor.i(x).i(y).double_value(&[]);
+            if objectness > 0.5 {
+                grid_points_with_objects.push(GridPointsWithObjects {
+                    x,
+                    y,
+                    prob: objectness,
+                })
+            }
+        }
+    }
+    grid_points_with_objects
+    //    let x_tensor = Tensor::of_slice(x_slice.as_slice());
+    //    let y_tensor = Tensor::of_slice(y_slice.as_slice());
+    //    x_tensor.print();
+    //    (x_tensor, y_tensor)
+}
+pub fn from_tensor(
+    tensor: R3TensorGeneric,
+    grid_size: u32,
+    original_img_size: u32,
+    anchors: (u32, u32),
+) -> Vec<Bbox> {
+    let tensor = tensor.tensor;
     let grid_size = grid_size as i64;
     let (anchor_width, anchor_height) = anchors;
     let grid_cell_size_pixels: i64 = (original_img_size / grid_size as u32) as i64;
@@ -118,14 +161,20 @@ pub fn from_tensor(tensor: Tensor, grid_size: u32, original_img_size: u32, ancho
     let class_1_prob = tensor.i(5 as i64);
     let mut bbs = vec![];
     for point in grid_points_with_objects {
-        let point_x_center = point.x*grid_cell_size_pixels + center_x_tensor.i(point.x).i(point.y).double_value(&[]) as i64;
-        let point_y_center = point.y*grid_cell_size_pixels + center_y_tensor.i(point.x).i(point.y).double_value(&[]) as i64;
-        let point_width = ((anchor_width as f64)*(width_tensor.i(point.x).i(point.y).double_value(&[])).exp()) as u32;
-        let point_height = ((anchor_height as f64)*height_tensor.i(point.x).i(point.y).double_value(&[]).exp()) as u32;
+        let point_x_center = point.x * grid_cell_size_pixels
+            + (center_x_tensor.i(point.x).i(point.y).double_value(&[])*grid_cell_size_pixels as f64) as i64;
+        let point_y_center = point.y * grid_cell_size_pixels
+            + (center_y_tensor.i(point.x).i(point.y).double_value(&[])*grid_cell_size_pixels as f64) as i64;
+        let point_width = ((anchor_width as f64)
+            * (width_tensor.i(point.x).i(point.y).double_value(&[])).exp())
+            as u32;
+        let point_height = ((anchor_height as f64)
+            * height_tensor.i(point.x).i(point.y).double_value(&[]).exp())
+            as u32;
         let point_class_1_prob = class_1_prob.i(point.x).i(point.y).double_value(&[]);
-        let top = (point_y_center as u32 - point_height/2) as i32;
-        let left = (point_x_center as u32 - point_width/2) as i32;
-//        let left = point_x_center
+        let top = (point_y_center - point_height as i64 / 2) as i32;
+        let left = (point_x_center - point_width as i64 / 2) as i32;
+        //        let left = point_x_center
         bbs.push(Bbox {
             top,
             left,
@@ -136,82 +185,73 @@ pub fn from_tensor(tensor: Tensor, grid_size: u32, original_img_size: u32, ancho
         });
     }
     bbs
-    // WARNING! Width, Height is not the real values, they are related by:
-    // Real Width = RW //// Real Height = RH
-    // RW = exp(Width) * anchor_width // RH = exp(Height) * anchor_width
-    // Width = ln(RW/anchor_width)
-    //    let rw = (bb.width as f32/anchor_width as f32).ln();
-    //    let height = (bb.height as f32/anchor_height as f32).ln();
-    //    // find out in which grid (x and y) the object center is
-    //    let bb_left_center_percentage = (bb.left as f32/original_img_size as f32); // something like 0.3
-    //    let grids_as_perc = (1./grid_size as f32);
-    //    let grids_to_left = (bb_left_center_percentage/grids_as_perc).floor() as u32;
-    //
-    //    let bb_top_center_percentage = (bb.top as f32/original_img_size as f32); // something like 0.3
-    //    let grids_to_top = (bb_top_center_percentage/grids_as_perc).floor() as u32;
-    //
-    //    tensor.i(0 as i64).i(grids_to_left as i64).i(grids_to_top as i64)
-    //        .copy_(&Tensor::from((bb.left as u32 - grids_to_left*grid_size) as f32));
-    //    tensor.i(1 as i64).i(grids_to_left as i64).i(grids_to_top as i64)
-    //        .copy_(&Tensor::from((bb.top as u32 - grids_to_top*grid_size) as f32));
-    //    tensor.i(2 as i64).i(grids_to_left as i64).i(grids_to_top as i64)
-    //        .copy_(&Tensor::from(width));
-    //    tensor.i(3 as i64).i(grids_to_left as i64).i(grids_to_top as i64)
-    //        .copy_(&Tensor::from(height));
-    //    tensor.i(4 as i64).i(grids_to_left as i64).i(grids_to_top as i64)
-    //        .copy_(&Tensor::from(1.));
-    //    tensor.i(5 as i64).i(grids_to_left as i64).i(grids_to_top as i64)
-    //        .copy_(&Tensor::from(1.));
-    //
-    //    tensor
+
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use image::imageops::resize;
+    use image::{FilterType, GenericImageView};
+    use std::env::var;
 
     #[test]
     fn test_to_tensor() {
         let grid_size = 13;
-        let original_img_size = 1220;
+        let original_img_size = 416;
         let anchors = (70, 70);
-        let bb = RawBbox {
+        let bb = Bbox {
             top: 10,
-            left: 600,
-            height: 20,
-            width: 20,
+            left: 200,
+            height: 25,
+            width: 30,
+            prob: 0.0,
+            class: "".to_string(),
         };
 
-        let bb2 = RawBbox {
+        let bb2 = Bbox {
             top: 100,
-            left: 610,
-            height: 20,
-            width: 20,
+            left: 250,
+            height: 36,
+            width: 40,
+            prob: 0.0,
+            class: "".to_string(),
         };
-        let tensor = to_tensor(vec![bb, bb2], 13, 1220, anchors);
-        tensor.print();
+        let tensor = to_tensor(vec![bb, bb2], grid_size, original_img_size, anchors);
+        tensor.tensor.print();
 
-        let a = from_tensor(tensor, grid_size, original_img_size, anchors);
-        println!("{:#?}", a);
+        let bbs = from_tensor(tensor, grid_size, original_img_size, anchors);
+        println!("{:#?}", bbs);
+    }
+    #[test]
+    fn convert_labels() {
+        let target_size = 416;
+        let output_path = "processed_imgs";
+        let labelboxs = read_labelbox_from_file(INPUT_FILE);
+        let mut labels: Vec<CleanedImgLabels> = vec![];
+        for label in labelboxs {
+            let img_path = format!("samples/{}", label.img_filename.clone());
+            let img = image::open(img_path).unwrap();
+            let (ori_width, ori_height) = (img.width(), img.height());
+            let width_ratio = (target_size as f32 / ori_width as f32);
+            let height_ratio = (target_size as f32 / ori_height as f32);
+            let resized = img.resize_exact(target_size, target_size, FilterType::CatmullRom);
+            resized
+                .save(format!("{}/{}", output_path, label.img_filename.clone()))
+                .unwrap();
+
+            let bb = convert_labelbox_to_only_bbs(&label, width_ratio, height_ratio);
+            labels.push(bb);
+
+
+        }
+        let output_label_file =
+            File::create(format!("{}/labels.json", output_path)).unwrap();
+        serde_json::to_writer_pretty(output_label_file, &labels);
     }
 }
-//fn main() {
-//    let labelbox = read_labelbox_from_file(INPUT_FILE);
-//    let cleaned_bb = convert_labelbox_to_only_bbs(labelbox);
-//
-//    let mut img: Blend<RgbaImage> = Blend(image::open("samples/image0000_a.jpg").unwrap().to_rgba());
-//
-//    let first_img = (&cleaned_bb[0]);
-//    for bb in first_img.bboxes.iter(){
-//        draw_bb_to_img(&mut img, bb);
-//    }
-//
-//
-//    img.0.save("output.jpg").unwrap();
-//
-//}
 
-pub fn draw_bb_to_img(img: &mut Blend<RgbaImage>, bb: &RawBbox) {
+pub fn draw_bb_to_img(img: &mut Blend<RgbaImage>, bb: &Bbox) {
     let rec = imageproc::rect::Rect::at(bb.left as i32, bb.top as i32)
         .of_size(bb.width as u32, bb.height as u32);
 
@@ -225,28 +265,26 @@ pub fn read_labelbox_from_file(filepath: &str) -> Vec<FileLabel> {
     serde_json::from_reader(input_file).unwrap()
 }
 
-pub fn convert_labelbox_to_only_bbs(file_labels: Vec<FileLabel>) -> Vec<CleanedImgLabels> {
-    let mut cleaned_img_labels = vec![];
-    for file in file_labels {
-        let img_filename = file.img_filename;
-        let objects = file.label.objects;
-        let bboxes: Vec<Bbox> = objects
-            .into_iter()
-            .map(|obj| Bbox {
-                top: obj.bbox.top,
-                left: obj.bbox.left,
-                height: obj.bbox.height,
-                width: obj.bbox.width,
-                prob: 1.0,
-                class: "0".to_string(),
-            })
-            .collect();
-        cleaned_img_labels.push(CleanedImgLabels {
-            img_filename,
-            bboxes,
-        });
-    }
-    cleaned_img_labels
-    //    let file = File::create(output_file_name).unwrap();
-    //    serde_json::to_writer_pretty(file, &cleaned_img_labels).unwrap();
+pub fn convert_labelbox_to_only_bbs(
+    file_labels: &FileLabel,
+    width_multiplier: f32,
+    height_multiplier: f32,
+) -> CleanedImgLabels {
+    let img_filename = &file_labels.img_filename;
+    let objects = &file_labels.label.objects;
+    let bboxes: Vec<Bbox> = objects
+        .into_iter()
+        .map(|obj| Bbox {
+            top: (obj.bbox.top as f32 * height_multiplier) as i32,
+            left: (obj.bbox.left as f32 * width_multiplier) as i32,
+            height: ((obj.bbox.height as f32) * height_multiplier) as u32,
+            width: ((obj.bbox.width as f32) * width_multiplier) as u32,
+            prob: 1.0,
+            class: "0".to_string(),
+        })
+        .collect();
+    return CleanedImgLabels {
+        img_filename: img_filename.clone(),
+        bboxes,
+    };
 }
